95f5d4172d93127eaf10304f280ff0017e32c7bc
miagrace06 <mcampanella@umail.ucsb.edu>
2024-10-24 14:08:57 -0700

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -15,30 +15,34 @@ int main() {
     cout << fixed << showpoint;
     cout << setprecision(2);
 
-    int size; 
+    int size;
+	// Input loop for number of grades 
     do {
 	    cout << "Enter number of grades: ";
 	    cin >> size;
+	    // Error check: size must be greater than 0
 	    if (size <=0){
 		    cout<< "Error!"<< endl;
-		    exit(1);
+		    exit(1); // Exit program if invalid input 
 	    }
 
-    }while (size <=0);
+    }while (size <=0); //Repeat until valid input 
 
-    int* grades = new int[size];
-    
-    cout << "Enter grades (each on a new line:" << endl;
+    int* grades = new int[size]; //Dynamically allocate an array for grades 
+
+	
+    cout << "Enter grades (each on a new line:" << endl; //Prompt user for grades input
     for (int i = 0; i < size; i++) {
-	    cin >> grades[i];
+	    cin >> grades[i]; //Read each grade
     }
+// Output statistics 	
     cout << "Here are some statistics:"<<endl;
     cout << "Average: " << average(grades, size) <<endl;
     cout <<"Median: " <<median(grades, size) << endl;
     cout<< "StdDev: " << stddev(grades, size) << endl;
-
+// Deallocate the dynamically allocated array 
     delete[] grades;
-    return 0;
+    return 0; //End of the program 
 }
 
 // This is bubbleSort - it is given for you to use.
@@ -56,29 +60,34 @@ void bubbleSort(int *array, int size) {
     } // for j
   } // for i
 }
-
+// Function to calculate the average of an array 
 double average(int *array, int size) {
 	double sum = 0;
 	for (int i = 0; i < size; i++){
-		sum += array[i];
+		sum += array[i]; // Sum of all grades 
 	}
-	return sum / size;
+	return sum / size; //Return average of array 
 }
+//Function to calculate the median of an array 
 double median (int* array, int size) {
-	bubbleSort(array, size);
+	bubbleSort(array, size); //Sort the array first 
 	if (size % 2 != 0) {
+		// If odd, return the middle element 
 		return array[size /2];
 	}
 	else {
+		//If even, return the average of the two middle elements 
 		return (array[(size - 1) / 2] + array[size / 2]) / 2.0;
 	}
 }
+//Function to calculate the standard deviation of an array
 double stddev (int* array, int size){
-	double mean = average(array, size);
+	double mean = average(array, size); //Calculate mean 
 	double sum_squares = 0;
 	for (int i = 0; i < size; i++) {
+		//Sum the squares of the differences from the mean 
 		sum_squares += pow(array[i] - mean, 2);
 	}
+	// Return the square root of the variance 
 	return pow(sum_squares / (size - 1), 0.5);
 }
-// MISSING FUNCTION DEFINITIONS HERE (you can remove these comments)
